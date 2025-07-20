ea8e5823b1b1fbe3cfbbba011ab573f1dafce0b1
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-20 19:43:30 +0300

    Supprimer une ligne vide dans le fichier de configuration de la base de données

--- src/configs/drizzle.ts
+++ src/configs/drizzle.ts
@@ -4,7 +4,6 @@ import { env } from "../lib/env"
 import * as schema from "@/db/schema"
 import { drizzle } from "drizzle-orm/node-postgres"
 
-
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
 })
