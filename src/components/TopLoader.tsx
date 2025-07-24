337e9c7956c93aca1650db109170e50cad2d072f
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-24 18:21:47 +0300

    Ajouter le composant TopLoader avec intégration de nextjs-toploader

---
+++ src/components/TopLoader.tsx
@@ -0,0 +1,25 @@
+"use client"
+import NextTopLoader from "nextjs-toploader"
+
+export default function TopLoader() {
+  /**
+   * ! STATE (état, données) de l'application
+   */
+  
+  
+  /**
+   * ! COMPORTEMENT (méthodes, fonctions) de l'application
+   */
+  
+  
+  /**
+   * ! AFFICHAGE (render) de l'application
+   */
+  return (
+   <NextTopLoader
+      color="#29d"
+      height={2.1}
+      showSpinner={false}   
+    />
+  )
+}
