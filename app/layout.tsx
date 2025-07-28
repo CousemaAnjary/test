64c106b575fa699af72cd996870a5f57ed1c847f
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-28 18:21:37 +0300

    Supprimer des lignes vides pour améliorer la lisibilité dans layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next"
 import TopLoader from "@/src/components/TopLoader"
 import { Geist, Geist_Mono } from "next/font/google"
 
-
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
