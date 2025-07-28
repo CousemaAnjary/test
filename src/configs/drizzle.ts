af63f4128428613223075d481d182f4951dcc480
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-28 21:37:29 +0300

    Ajouter une ligne vide pour améliorer la lisibilité dans drizzle.ts

--- src/configs/drizzle.ts
+++ src/configs/drizzle.ts
@@ -4,6 +4,7 @@ import * as schema from "@/db/schema"
 import { DATABASE_URL } from "./database"
 import { drizzle } from "drizzle-orm/node-postgres"
 
+
 const pool = new Pool({
   connectionString: DATABASE_URL,
 })
