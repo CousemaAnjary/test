3bd1360f911a2af29784f91896fd38b2dfebf14d
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-29 18:42:44 +0300

    Ajouter des lignes vides pour améliorer la lisibilité dans page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,9 +3,11 @@ export default function LoginPage() {
    * ! STATE (état, données) de l'application
    */
   
+
   /**
    * ! COMPORTEMENT (méthodes, fonctions) de l'application
    */
+  
   
   /**
    * ! AFFICHAGE (render) de l'application
