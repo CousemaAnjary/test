0c674a6e45af6d2b29a8ad5fc4a146bb15b3311c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-08-09 20:38:36 +0300

    Supprimer une ligne vide pour améliorer la lisibilité dans page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,11 +2,11 @@ export default function LoginPage() {
   /**
    * ! STATE (état, données) de l'application
    */
-
   
   /**
    * ! COMPORTEMENT (méthodes, fonctions) de l'application
    */
+
 
 
   /**
